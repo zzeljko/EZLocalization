7dfa2289d9ea9467ae945c3e9e54e4037493d9cf
zzeljko <zurzic_doru@yahoo.com>
2018-04-18 13:21:35 +0300

    implemented gps detection as service

---
+++ app/src/main/java/com/example/zeljko/ezclient/GpsScannerService.java
@@ -0,0 +1,88 @@
+package com.example.zeljko.ezclient;
+
+import android.Manifest;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.provider.Settings;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
+
+/**
+ * Created by zeljko on 21.03.2018.
+ */
+
+public class GpsScannerService extends Service {
+
+    private static final int MIN_TIME_BETWEEN_SCANS = 1000;
+    private static final int MIN_DISTANCE_BETWEEN_SCANS = 0;
+
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+
+        locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
+        createLocationListener();
+
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return ;
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BETWEEN_SCANS,
+                MIN_DISTANCE_BETWEEN_SCANS, locationListener);
+    }
+
+    private void createLocationListener() {
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Intent intent = new Intent("location_update");
+                intent.putExtra("location", location.getLatitude() + " " + location.getLongitude());
+
+                sendBroadcast(intent);
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(intent);
+            }
+        };
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null)
+            locationManager.removeUpdates(locationListener);
+    }
+}
--- app/src/main/java/com/example/zeljko/ezclient/MainActivity.java
+++ app/src/main/java/com/example/zeljko/ezclient/MainActivity.java
@@ -1,42 +1,54 @@
 package com.example.zeljko.ezclient;
 
-import android.location.Location;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import java.util.List;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity implements IGpsDataCallback {
-
-    private GpsScanner gpsScanner;
     private WiFiScanner wiFiScanner;
     private CommunicationChannel channel;
+    private BroadcastReceiver gpsBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        gpsScanner = new GpsScanner(this, this);
         wiFiScanner = new WiFiScanner();
         channel = new CommunicationChannel();
+
+        Intent intent = new Intent(getApplicationContext(), GpsScannerService.class);
+        startService(intent);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (gpsBroadcastReceiver == null)
+            gpsBroadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Toast.makeText(getApplicationContext(), intent.getExtras().get("location").toString(), Toast.LENGTH_SHORT).show();
+                }
+            };
+
+        registerReceiver(gpsBroadcastReceiver, new IntentFilter("location_update"));
     }
 
     @Override
     protected  void onDestroy() {
         super.onDestroy();
-        gpsScanner.remove();
-    }
 
-    @Override
-    public void onGpsAvailabilityChange(boolean avail) {
-        if (!avail)
-            Toast.makeText(this, "GPS not avail", Toast.LENGTH_SHORT).show();
-    }
+        Intent intent = new Intent(getApplicationContext(), GpsScannerService.class);
+        stopService(intent);
 
-    @Override
-    public void onGpsLocationChanged(Location location) {
-        Toast.makeText(this, location.toString(), Toast.LENGTH_SHORT).show();
+        if (gpsBroadcastReceiver != null)
+            unregisterReceiver(gpsBroadcastReceiver);
     }
 }
